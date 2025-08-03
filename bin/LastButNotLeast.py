f1f228d15a5088c005152e54e59c09e0c8ebedd6
Julie Cailler <julie_cailler@hotmail.fr>
2025-08-03 20:28:07 +0200

    add sources

---
+++ bin/LastButNotLeast.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+print("% LastButNotLeast: The fastest way to give up!")
+print("% For best results, do not expect results.")
+print("% SZS status GaveUp")
+print("% It's not a bug — it's a philosophical stance.")
+print("% Thanks for trying LastButNotLeast :)")
---
+++ sample_solution.p
@@ -0,0 +1,5 @@
+% LastButNotLeast: The fastest way to give up!
+% For best results, do not expect results.
+% SZS status GaveUp
+% It's not a bug — it's a philosophical stance.
+% Thanks for trying LastButNotLeast :)
